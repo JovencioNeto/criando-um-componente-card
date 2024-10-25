96db89513106a83a229c3c417d403a0ef46a120c
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2024-10-25 11:48:54 +0000

    Minha atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 export default function App(){
   return(
     <div className="container">
-      <img src="./public.star.png" alt="" />
+      <img src="./public/stars.jpg" alt="" />
 
       <div className="text-content">
-        <h1>Pôster: Star Wars (1977)</h1>
+        <strong><h1>Pôster: Star Wars (1977)</h1></strong>
         <p>
         Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. 
         Um ótima recordação de um dos mais icônicos filmes de todos os tempos.Este clássico
